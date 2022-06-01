766f845b5f9bd531f9c02967c2b1c39c75ec0796
porameht <porameht@outlook.com>
2022-06-01 16:29:37 +0700

    import swaggerui and write swagger in file books

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const low = require("lowdb");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const booksRouter = require("./routes/books");
 
 const PORT = process.env.PORT || 4000;
@@ -13,13 +15,31 @@ const db = low(adapter);
 
 db.defaults({ books: [] }).write();
 
+const option = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(option);
 const app = express();
 
 app.db = db;
-
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
 
+app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/books", booksRouter);
--- routes/books.js
+++ routes/books.js
@@ -4,15 +4,95 @@ const { nanoid } = require("nanoid");
 
 const idLength = 8;
 
-router.get("/", (req, res) => {
-  const books = req.app.db.get("books");
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     book:
+ *       type: object 
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generate id of the book
+ *         title:
+ *           type: string
+ *           description: The book title
+ *         author:
+ *           type: string
+ *           description: The book author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         author: fr4nk k.
+ */
 
-  res.send(books);
+ /**
+  * @swagger
+  * tags:
+  *   name: Books
+  *   description: The books managing API
+  */
+
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: The list of the books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ */
+
+
+router.get("/", (req, res) => {
+	const books = req.app.db.get("books");
+
+	res.send(books);
 });
+
+/**
+ * @swagger
+ * /books/{id}:
+ *   get:
+ *     summary: Get the book by id
+ *     tags: [Books]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The book id
+ *     responses:
+ *       200:
+ *         description: The book description by id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ *       404:
+ *         description: The book was not found
+ */
+
+
+
 
 router.get("/:id", (req, res) => {
   const book = req.app.db.get("books").find({ id: req.params.id }).value();
 
+  if(!book){
+    res.sendStatus(404)
+  }
   res.send(book);
 });
 
